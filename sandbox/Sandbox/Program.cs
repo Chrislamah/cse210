0053031e4dc8e7fc5d719811653eaadcf6b56ea2
Chrislamah <kouwontor25@gmail.com>
2024-09-21 21:09:56 +0300

    remove name

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World chris Lamah!!");
+        Console.WriteLine("Hello Sandbox World chris-Lamah!");
     }
 }
